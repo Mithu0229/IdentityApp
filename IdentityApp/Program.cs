dfb3bac6118f1874c67d390526840c71af8daeb3
Mithu0229 <showkat131080@gmail.com>
2023-11-23 22:52:41 +0600

    cookieAuth program and login page

--- IdentityApp/Program.cs
+++ IdentityApp/Program.cs
@@ -3,6 +3,12 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 
+builder.Services.AddAuthentication("MyCookieAuth").AddCookie("MyCookieAuth", options =>
+{
+    options.Cookie.Name = "MyCookieAuth";
+    options.ExpireTimeSpan = TimeSpan.FromSeconds(200);
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
